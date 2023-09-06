7089d6ebdf33910103922acc55b403a5c8df3979
Neo Wang <43394822+nwatx@users.noreply.github.com>
2023-09-06 13:14:13 -0500

    add behaivor cloning

---
+++ run.sh
@@ -0,0 +1,4 @@
+python libero/lifelong/main.py seed=SEED \
+                               benchmark_name=BENCHMARK \
+                               policy=POLICY \
+                               lifelong=ALGO
