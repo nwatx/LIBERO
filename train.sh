ec21aa0aa3646071025224d3be6b1fe14cf93d55
Neo Wang <43394822+nwatx@users.noreply.github.com>
2023-09-20 02:22:49 -0500

    Update train.sh

--- train.sh
+++ train.sh
@@ -8,4 +8,4 @@ fi
 
 pip install -r requirements.txt
 pip install torch==1.11.0+cu113 torchvision==0.12.0+cu113 torchaudio==0.11.0 --extra-index-url https://download.pytorch.org/whl/cu113
-python libero/lifelong/main.py benchmark_name=LIBERO_OBJECT policy=bc_transformer_policy lifelong=base
+python3 libero/lifelong/main.py benchmark_name=LIBERO_OBJECT policy=bc_transformer_policy lifelong=base
